7dab4bff34df9950f41739a600af8ce87d53da7c
Manuel Márquez <punketo28@gmail.com>
2015-03-02 15:33:38 -0430

    README improved

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # odoo-addons
 Our contributions to Odoo.
+
+## workflows_logs Module
+
+This module allows you to record changes in the workflows of objects in a model, for it must create a trace for the model and activate it. From then automatically begin to record the changes. The logs can see them through the Reporting/Workflows Logs/Workflows Logs menu.
+
+Created Menus:
+
+* Reporting/Workflows Logs 
+* Reporting/Workflows Logs/Workflows Logs 
+* Reporting/Workflows Logs/Workflows Traces
--- workflows_logs/__openerp__.py
+++ workflows_logs/__openerp__.py
@@ -28,7 +28,15 @@
     'website': 'hoatzin.org',
     'description':
         u"""
-        Logs of Workflows
+Logs of Workflows
+=================
+This module allows you to record changes in the workflows of objects in a model, for it must create a trace for the model and activate it. From then automatically begin to record the changes. The logs can see them through the Reporting/Workflows Logs/Workflows Logs menu.
+
+Created Menus:
+
+* Reporting/Workflows Logs 
+* Reporting/Workflows Logs/Workflows Logs 
+* Reporting/Workflows Logs/Workflows Traces
         """,
     'depends': [
         'base'
